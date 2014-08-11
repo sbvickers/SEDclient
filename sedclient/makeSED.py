2051eb3ad056b399733a9537c70218d22b5c286f
sbvickers <shane.vickers95@gmail.com>
2014-08-11 13:52:40 +1000

    Added makeSED.py file where the actual building of the SED using the SED class takes place.

---
+++ sedclient/makeSED.py
@@ -0,0 +1,101 @@
+from sedclient import sedPlot
+from sedclient import globs
+
+def make():
+    """
+        The function for building an SED for an object. Takes no inputs as they
+        are taken from the globals 'globs' file.
+
+        Parameters
+        ----------
+                None
+
+        Returns
+        ----------
+                None
+    """
+
+    openLogger()
+
+    # builds SED skeleton
+    SED = sedPlot.Plot()
+
+    # plots the data
+    SED.plotPh()
+    SED.plotSp()
+
+    # ------------------------------------------- #
+    # space here to call a fitting function and 
+    # return some data to plot on the SED.
+    # ------------------------------------------- #
+
+    # standard annotations; name, ra, dec, ebv
+    for annotation in makeAnns():
+        SED.annotate(annotation)
+
+    SED.legend()
+
+    SED.saveSed()
+
+    SED.show()
+
+    closeLogger()
+
+def makeAnns():
+    """
+        Prepares the strings for the Annotations.
+
+        Parameters
+        ----------
+                None
+
+        Returns
+        ----------
+                None
+    """
+    annotations = []
+
+    annotations.append("$\\rm{{ {} }}$".format(globs.name))
+    coordStr = "$l={:.3F}^\circ, \,b={:.3F}^\circ$".format(globs.l, globs.b)
+    annotations.append(coordStr)
+    annotations.append("$\\rm{{ E(B-V) }}={}\pm{}\,\\rm{{ mag }}$".format(globs.ebv.n, globs.ebv.s))
+
+    return annotations
+
+def openLogger():
+    """
+        Opens a log file for each object.
+
+        Parameters
+        ----------
+                None
+
+        Returns
+        ----------
+                None
+    """
+    # sets up logging file for each object
+    globs.objectHandler = globs.logging.FileHandler("{}{}.log".format(globs.dirLog, globs.name.replace(' ', '_')))
+    globs.objectHandler.setLevel(globs.logging.DEBUG)
+    globs.objectHandler.setFormatter(fmt=globs.logFormat)
+
+    # adds new logging file to logger
+    globs.logger.addHandler(globs.objectHandler)
+
+    globs.logger.info("Building SED for {}, added objectHandler to logger.".format(globs.name))
+
+def closeLogger():
+    """
+        Closes the log file for each object.
+
+        Parameters
+        ----------
+                None
+
+        Returns
+        ----------
+                None
+    """
+    globs.logger.info("Finished building SED for {}, and removed objectHandler from logger.".format(globs.name))
+
+    globs.logger.removeHandler(globs.objectHandler)
